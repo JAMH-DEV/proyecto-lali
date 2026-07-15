2946391b063388fdf60b8e805b0bf2ad4bfb9acd
JAMH <mascota_23joshua@outlook.com>
2026-07-15 15:36:31 -0600

    Ajuste de list

--- services/minecraft/app/schemas/response.py
+++ services/minecraft/app/schemas/response.py
@@ -1,6 +1,12 @@
 from pydantic import BaseModel
 
 
+class CommandResponse(BaseModel):
+    success: bool
+    command: str
+    response: str
+
+
 class PlayersResponse(BaseModel):
     success: bool
     online: int
